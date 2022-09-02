5b83d95e6b6590d430fb8f87090104eca47fdf67
Kaiserovich <pkirill281999@gmail.com>
2022-09-02 12:26:25 +0300

    First ok start

--- My company/Program.cs
+++ My company/Program.cs
@@ -5,11 +5,20 @@ namespace My_company
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseStaticFiles();
+
+            app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
     }
 }
